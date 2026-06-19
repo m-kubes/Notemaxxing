797e8e9e59692d274b6b3b9ddbea69c0f8e0fcfa
kubes05 <michaelkubiak5@gmail.com>
2026-06-19 16:17:47 -0400

    Production build works now

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require("cors");
+const path = require('path')
 
 const notesRouter = require('./routes/notesRouter');
 const badRequestHandler = require('./middleware/badRequestHandler');
@@ -7,17 +8,34 @@ const rateLimiter = require('./middleware/rateLimiter');
 const {connectDB} = require('../src/config/db');
 
 const app = express();
+const __localDirname = path.resolve("../frontend/dist")
 
-// middleware
-app.use(cors({
-    origin: 'http://localhost:5173'
-}));
+
+// use cors in development build because we're on 2 different ports
+if (process.env.NODE_ENV !== 'production') {
+    app.use(cors({
+        origin: 'http://localhost:5173'
+    }));
+}
+
+
+// other middleware
 app.use(express.json());
 app.use(rateLimiter)
 app.use(badRequestHandler);
 
 // routes
 app.use('/api/notes', notesRouter);
+
+// serve index.html in production build
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__localDirname)))
+
+    app.get("*path", (req, res) => {
+        console.log(__localDirname)
+        res.sendFile(path.join(__localDirname, 'index.html'));
+    })
+}
 
 // connect to mongoDB and then start listening
 connectDB().then(() => {
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+// there isnt a localhost in production
+const BASE_URL = import.meta.env.MODE === 'development' ? "http://localhost:5050/api" : "/api"
+
 const api = axios.create({
-    baseURL: "http://localhost:5050/api"
+    baseURL: BASE_URL,
 })
 
 export default api;
